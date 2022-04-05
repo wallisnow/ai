5450e8a4fdce33d30c78f6544156e9ee49e2bfe4
jun <lj@crrc.tech>
2022-04-05 15:02:35 +0800

    执行算法代码之前，将用户上传的zip文件，解压缩到algos文件夹，并用当前算法的id重命名文件夹。 如果当前算法id已存在同名文件夹，则删除已存在的

--- src/main/java/com/ai/sys/train/SimpleDatasetProcessor.java
+++ src/main/java/com/ai/sys/train/SimpleDatasetProcessor.java
@@ -3,26 +3,37 @@ package com.ai.sys.train;
 import com.ai.sys.model.entity.Algo;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.FileSystemUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 @Slf4j
 public class SimpleDatasetProcessor implements DatasetProcessor {
+
+    @Value("workspace.algo")
+    private String algoDir;
 
     private final static String PYTHON_EXECUTOR = "python";
     private final static String PARAM_SPLITER = " ";
 
     @Override
     public void process(@NonNull Algo algo, List<String> params) {
+        //TODO jun: to test this method
+        try {
+            unzipSourceCode(algo);
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
+
         List<String> paramList = Optional.ofNullable(params).orElseGet(ArrayList::new);
         paramList.add(algo.getDataSet().getPath());
         ProcessBuilder processBuilder = new ProcessBuilder(PYTHON_EXECUTOR, algo.getPath(), String.join(PARAM_SPLITER, paramList));
@@ -46,5 +57,53 @@ public class SimpleDatasetProcessor implements DatasetProcessor {
                     log.error(throwable.getMessage());
                     return null;
                 });
+    }
+
+    private void unzipSourceCode(Algo algo) throws IOException {
+        File destDir = new File(algoDir+'/'+algo.getId());
+        if(destDir.exists())
+            FileSystemUtils.deleteRecursively(destDir);
+
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(algo.getPath()));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = newFile(destDir, zipEntry);
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                // fix for Windows-created archives
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+
+                // write file content
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+    }
+
+    private static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
     }
 }
