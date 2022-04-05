66f6d46654759f7b4b72a0859cd5b01e0e8777d2
jun <lj@crrc.tech>
2022-04-05 14:11:38 +0800

    更新数据库测试数据

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,19 +4,19 @@ ON CONFLICT (role_name) DO NOTHING;
 
 
 
-INSERT INTO algo VALUES (1, '2022-01-01 00:00:00', '2022-01-01 00:00:00', 'test1', 'test1', '/test/1', 1, 1, 1);
+INSERT INTO algo VALUES (1, '2022-01-01 00:00:00', '2022-01-01 00:00:00', 'test1', 1, 'test1', '/Users/liujun/Desktop/AI/algos/1/print.py', 1, 1, 1,NULL);
 
 
 INSERT INTO category VALUES (1, '2022-01-01 00:00:00', '2022-01-01 00:00:00', '图像识别', '图像识别');
 
 
-INSERT INTO data_set VALUES (1, '2022-01-01 00:00:00', '2022-01-01 00:00:00', 'ImageNet', '/dataset/imagenet', 1);
+INSERT INTO data_set VALUES (1, '2022-01-01 00:00:00', '2022-01-01 00:00:00', 'ImageNet', '/Users/liujun/Desktop/AI/datasets/heart.csv', 1);
 
 
 INSERT INTO menu_role VALUES (1, 1);
 
 
-INSERT INTO sys_menu VALUES (1, '2022-01-01 00:00:00', '2022-01-01 00:00:00', 1, NULL, '/icon', 'test', 'sys:admin', 1, 0, 1, '/test', NULL);
+INSERT INTO sys_menu VALUES (1, '2022-01-01 00:00:00', '2022-01-01 00:00:00', TRUE, NULL, '/icon', 'test', 'sys:admin', 1, 0, 1, '/test', NULL);
 
 
 INSERT INTO sys_role VALUES (1, '2022-01-01 00:00:00', '2022-01-01 00:00:00', 'admin', 'ADMIN');
